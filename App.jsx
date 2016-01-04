84de7b057cd26297120e222cb60c6024e9903d4a
Justyna Rachowicz <mrowa44@gmail.com>
2016-01-04 19:42:47 +0100

    Keep tasks in db

--- App.jsx
+++ App.jsx
@@ -1,16 +1,17 @@
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "Task 1" },
-      { _id: 2, text: "Task 2" },
-      { _id: 3, text: "Task 3" }
-    ];
+  mixins: [ReactMeteorData],
+
+  // this.data.tasks
+  getMeteorData() {
+    return { tasks: Tasks.find({}).fetch() }
   },
+
   renderTasks() {
-    return this.getTasks().map((task) => {
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
+
   render() {
     return (
       <div className="container">
