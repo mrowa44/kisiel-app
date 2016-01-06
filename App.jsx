35fe3de57994ee1364d250eae70441530f84982c
Justyna Rachowicz <mrowa44@gmail.com>
2016-01-06 15:40:24 +0100

    Add hide completed and undone tasks count

--- App.jsx
+++ App.jsx
@@ -1,9 +1,24 @@
 App = React.createClass({
   mixins: [ReactMeteorData],
 
+  getInitialState() {
+    return {
+      hideCompleted: false
+    }
+  },
+
   // this.data.tasks
   getMeteorData() {
-    return { tasks: Tasks.find({}, { sort: {createdAt: -1}}).fetch() }
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      query = {checked: {$ne: true}}
+    }
+
+    return {
+      tasks: Tasks.find(query, { sort: {createdAt: -1}}).fetch(),
+      undoneCount: Tasks.find({checked: {$ne: true}}).count()
+}
   },
 
   renderTasks() {
@@ -19,11 +34,25 @@ App = React.createClass({
     ReactDOM.findDOMNode(this.refs.textInput).value = "";
   },
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  },
+
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo list</h1>
+          <h1>Todo list ({this.data.undoneCount})</h1>
+            <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+              Hide Completed Tasks
+          </label>
             <form className="new-task" onSubmit={this.handleSubmit} >
             <input type="text" ref="textInput" placeholder="Type to add new tasks" />
           </form>
