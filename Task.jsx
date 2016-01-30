d887b8bd3460bcfc31b88e881b66ce84c5debb55
Justyna Rachowicz <mrowa44@gmail.com>
2016-01-30 05:02:55 +0100

    Add private to tasks

--- Task.jsx
+++ Task.jsx
@@ -1,10 +1,15 @@
 Task = React.createClass({
   propTypes: {
-    task: React.PropTypes.object.isRequired
+    task: React.PropTypes.object.isRequired,
+    showPrivateButton: React.PropTypes.bool.isRequired
   },
 
   toggleChecked() {
     Meteor.call("setChecked", this.props.task._id, ! this.props.task.checked);
+  },
+
+  togglePrivate() {
+    Meteor.call("setPrivate", this.props.task._id, ! this.props.task.private);
   },
 
   deleteThisTask() {
@@ -12,7 +17,8 @@ Task = React.createClass({
   },
 
   render() {
-    const taskClassName = this.props.task.checked ? "checked" : "";
+    const taskClassName = (this.props.task.checked ? "checked" : "") + " " +
+          (this.props.task.private ? "private" : "");
     return (
       <li className={taskClassName}>
         <button className="delete" onClick={this.deleteThisTask}>
@@ -23,6 +29,13 @@ Task = React.createClass({
           readOnly={true}
           checked={this.props.task.checked}
           onClick={this.toggleChecked} />
+
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate}>
+            { this.props.task.private ? "Private" : "Public" }
+          </button>
+        ) : ''}
+
          <span className="text">
             {this.props.task.text} <em>~{this.props.task.username}</em>
          </span>
